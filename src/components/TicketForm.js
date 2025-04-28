6f27f20d08661ecf37fd093f0535012115543ebe
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-04-28 22:28:36 +0300

    Fix the import

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, use } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function TicketForm({ dispatch, editingTicket }) {
   const [title, setTitle] = useState('');
